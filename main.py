e79faa79dc9fe915e0ff98cbea6d4ef265cfc61c
d4 r c k h <m4dd0x.xxx@gmail.com>
2021-04-02 14:37:30 +0300

    run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 
 cachierDir = '{}/.cachier'.format(os.path.expanduser("~"))
 
@@ -6,3 +7,19 @@ cachierDir = '{}/.cachier'.format(os.path.expanduser("~"))
 if not os.path.exists(cachierDir):
     print('WARN: "{}" is not present, creating it now'.format(cachierDir))
     os.makedirs(cachierDir)
+
+group = 'default'
+groupDir = '{}/{}'.format(cachierDir, group)
+
+# making sure the group directory exists in cachierDir
+if not os.path.exists(groupDir):
+    print('WARN: "{}" is not present, creating it now'.format(groupDir))
+    os.makedirs(groupDir)
+
+if sys.argv[1] == 'run':
+    # run command in sys.argv[2]
+    command = sys.argv[2]
+    commandName = command.split(" ")[0]
+    print('running "' + command + '". command name is: ' + commandName)
+    # save output in groupDir/{commandName}.txt
+    # save commandName, command args and file path to groupDir/{commandName}.json
