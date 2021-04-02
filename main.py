a191bea55ac24c38e4b576068c8d39a54d87dce9
d4 r c k h <m4dd0x.xxx@gmail.com>
2021-04-02 16:28:45 +0300

    date

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import os.path, os
 import sys
+import datetime
 
 cachierDir = '{}/.cachier'.format(os.path.expanduser("~"))
-counter = 1
-
-def uniquify(path):
-    global counter
-    filename, extension = os.path.splitext(path)
-    while os.path.exists(path):
-        path = filename + extension + "." + counter
-        counter+=1
-    return path
 
 # making sure the .cachier directory is present
 if not os.path.exists(cachierDir):
@@ -27,12 +19,10 @@ if not os.path.exists(groupDir):
 
 if sys.argv[1] == 'run':
     # run command in sys.argv[2]
-    command = uniquify(sys.argv[2])
+    command = sys.argv[2]
     commandName = command.split(" ")[0]
-    if os.path.isfile(f'{groupDir}/{command}.json'):
-        os.system(f"{uniquify(f'{command}')} | tee {groupDir}/{command}.json.{str(counter)}")
-    else:
-        os.system(f"{command} | tee {groupDir}/{command}.json")
-    print('running "' + command + '". command name is: ' + commandName)
+    outputFile = commandName + "_" + datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+    os.system(f"{command} | tee \"{groupDir}/{outputFile}.txt\"")
+    #print('running "' + command + '". command name is: ' + commandName)
     # save output in groupDir/{commandName}.txt
     # save commandName, command args and file path to groupDir/{commandName}.json
