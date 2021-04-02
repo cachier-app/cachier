f3354ebee2f8893f7ca583e42e48f5517e589170
Rc <60568652+HACKE-RC@users.noreply.github.com>
2021-04-02 18:41:33 +0530

    Create main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import os.path
+import os.path, os
 import sys
 
 cachierDir = '{}/.cachier'.format(os.path.expanduser("~"))
+counter = 1
+
+def uniquify(path):
+    global counter
+    filename, extension = os.path.splitext(path)
+    while os.path.exists(path):
+        path = filename + extension + "." + counter
+        counter+=1
+    return path
 
 # making sure the .cachier directory is present
 if not os.path.exists(cachierDir):
@@ -18,8 +27,12 @@ if not os.path.exists(groupDir):
 
 if sys.argv[1] == 'run':
     # run command in sys.argv[2]
-    command = sys.argv[2]
+    command = uniquify(sys.argv[2])
     commandName = command.split(" ")[0]
+    if os.path.isfile(f'{groupDir}/{command}.json'):
+        os.system(f"{uniquify(f'{command}')} | tee {groupDir}/{command}.json.{str(counter)}")
+    else:
+        os.system(f"{command} | tee {groupDir}/{command}.json")
     print('running "' + command + '". command name is: ' + commandName)
     # save output in groupDir/{commandName}.txt
     # save commandName, command args and file path to groupDir/{commandName}.json
